088cff31d0d47b8c5c92394957cb86663ad3c95a
yavuzim <kyavuzimer@gmail.com>
2023-08-25 15:54:47 +0300

    Kitap islemleri yapildi.

--- MVCLibrary/Controllers/BookController.cs
+++ MVCLibrary/Controllers/BookController.cs
@@ -23,19 +23,53 @@ namespace MVCLibrary.Controllers
         [HttpGet]
         public ActionResult BookAdd()
         {
+            ViewBag._authorList = GetSelectListItems<Author>(dbLibraryEntities.Author.ToList(),
+                author => author.firstName + ' ' + author.lasName,
+                author => author.authorId.ToString());
             ViewBag._categoryList = GetSelectListItems<Category>(dbLibraryEntities.Category.ToList(),
                 category => category.categoryName,
                 category => category.categoryId.ToString());
-            ViewBag._authorList = GetSelectListItems<Author>(dbLibraryEntities.Author.ToList(),
-                author => author.firstName,
-                author => author.authorId.ToString());
             return View();
         }
         [HttpPost]
         public ActionResult BookAdd(Book book)
         {
-
-            return View();
+            var category = dbLibraryEntities.Category.Where(p => p.categoryId == book.categoryId).FirstOrDefault();
+            var author = dbLibraryEntities.Author.Where(p => p.authorId == book.authorId).FirstOrDefault();
+            book.Category = category;
+            book.Author = author;
+            dbLibraryEntities.Book.Add(book);
+            dbLibraryEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BookDelete(int id)
+        {
+            dbLibraryEntities.Book.Remove(dbLibraryEntities.Book.Find(id));
+            dbLibraryEntities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BookGet(int id)
+        {
+            var book = dbLibraryEntities.Book.Find(id);
+            ViewBag._authorList = GetSelectListItems<Author>(dbLibraryEntities.Author.ToList(),
+               author => author.firstName + ' ' + author.lasName,
+               author => author.authorId.ToString());
+            ViewBag._categoryList = GetSelectListItems<Category>(dbLibraryEntities.Category.ToList(),
+                category => category.categoryName,
+                category => category.categoryId.ToString());
+            return View("BookGet", book);
+        }
+        public ActionResult BookUpdate(Book updateBook)
+        {
+            var book = dbLibraryEntities.Book.Find(updateBook.bookId);
+            book.bookName = updateBook.bookName;
+            book.Author = dbLibraryEntities.Author.Where(p => p.authorId == updateBook.authorId).FirstOrDefault();
+            book.Category = dbLibraryEntities.Category.Where(p => p.categoryId == updateBook.categoryId).FirstOrDefault();
+            book.publicationYear = updateBook.publicationYear;
+            book.publishingHouse = updateBook.publishingHouse;
+            book.pageNumber = updateBook.pageNumber;
+            dbLibraryEntities.SaveChanges();
+            return RedirectToAction("Index");
         }
         public List<SelectListItem> GetSelectListItems<TEntity>(List<TEntity> entities, Func<TEntity, string> textSelector, Func<TEntity, string> valueSelector)
         {
